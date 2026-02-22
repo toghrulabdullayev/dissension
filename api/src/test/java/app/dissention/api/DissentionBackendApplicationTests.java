bf00f129db8ddbceb0c32864e65a66830a0d3c85
toghrulabdullayev <toghrulabdullayev1@gmail.com>
2026-02-22 16:38:46 +0400

    publish initial backend

---
+++ api/src/test/java/app/dissention/api/DissentionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.dissention.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissentionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
